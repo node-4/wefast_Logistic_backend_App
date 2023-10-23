e6449e2c498deda461691c7401f8e49a73612889
rubiyadav <rubiflyeis@gmail.com>
2023-10-23 13:08:02 +0530

    update

--- src/middlewares/validators/user/update-details.js
+++ src/middlewares/validators/user/update-details.js
@@ -3,7 +3,7 @@ const { getBodyValidationMiddleware } = require("../validators.js");
 
 const schema = Joi.object({
     name: Joi.string().min(2).max(20).optional(),
-    profileImage: Joi.string().uri()
+    profile_image: Joi.string().uri()
 });
 
 exports.updateDetails = getBodyValidationMiddleware(schema);
